562e5472b84dac89b4c427c37faa5e88b5c24cdd
limonatasuyu <emredilek6@gmail.com>
2022-05-01 16:17:07 +0300

    Finished getting data from api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import { useState/*, useEffect*/ } from 'react'
+import { useState, useEffect} from 'react'
+import defaultImg from './img/default.jpeg'
+import sunnyImg from './img/sunny.jpeg'
 import cloudyImg from './img/cloudy.jpeg'
+import lightRainImg from './img/lightRain.jpeg'
+import rainyImg from './img/rainy.jpeg'
 import './animations.css'
+import fetchDataFunction from './fetchData'
 
-//const API_KEY = "cda7151f40e144b3a91153640222804"
 
 function ContentContainer(props) {
+
+	let weatherImg = defaultImg;
+	
+	// Checks if the data fetched at the time
+	if (props.data) {
+		if (props.data.currentWeather === "Sunny") {weatherImg = sunnyImg}
+		if (props.data.currentWeather === "Light rain") {weatherImg = lightRainImg}
+		if (props.data.currentWeather === "Rainy") {weatherImg = rainyImg}
+		if (props.data.currentWeather === "Partly cloudy" || props.data.currentWeather === "cloudy") {weatherImg = cloudyImg}
+	}
 	return(
 		<div className={props.className + " content--container"} onClick={props.onClick}>
-			<img className="weather-img" src={cloudyImg} alt="some" />
+			<img className="weather-img" src={weatherImg} alt="some" />
 			<div className="text-content">
-				<h1>London</h1>
-				<p>Condition: cloudy</p>
-				<p>Temp(C): 14.0</p>
-				<p>Date: 28.04.2022</p>
+				<h1>{props.data && props.data.name}</h1>
+				<p>Condition: {props.data && props.data.currentWeather}</p>
+				<p>Temp(C): {props.data && props.data.temp_c}</p>
+				<p>Date: {props.data && props.data.date}</p>
 			</div>
 		</div>
 	)
 }
 
 export default function App() {
-
-	//const [data, setData] = useState({})
-
-	//useEffect(() => {
-	//	fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=London&days=1&aqi=no&alerts=no`)
-	//		.then(res => res.json())
-	//		.then(data => setData(data))
-	//}, [])
-	//var condition = data.current.condition.text
 	
 	const [containerAnims, setAnims] = useState({
 		animation: " ",
@@ -55,29 +60,39 @@ export default function App() {
 			rightNone: "container-right--none",
 		}
 	
-	
-	function handleAnimation(event) {
+	function handleClick(event) {
 
 		let oldClasses = {...containerAnims}
 		let newClasses;
+		let newData;
 		
-		if (event.pageX > 120 && event.pageX < 650)        {newClasses = leftClickAnims}
-		else if (event.pageX > 1270 && event.pageX < 1790) {newClasses = rightClickAnims}
+		const leftClickData = [data[4], ...data.slice(0, 4)]
+		const rightClickData = [...data.slice(1,5), data[0]] 
+		
+		if (event.pageX > 120 && event.pageX < 650)        {newClasses = leftClickAnims;  newData=leftClickData}
+		else if (event.pageX > 1270 && event.pageX < 1790) {newClasses = rightClickAnims; newData=rightClickData}
 		else {return}
 
 		setAnims(newClasses)
-		setTimeout(() => {setAnims(oldClasses)}, 2000)
+		setTimeout(() => {setAnims(oldClasses); setData(newData)}, 2000)
 	}
+	
+	const [data, setData] = useState({})
+
+	useEffect(() => {
+		fetchDataFunction()
+			.then(data => setData(data))
+	}, [])
 
 	return(
 		<div className="App">
 			<h1 className="title">Current Weather</h1>
 			<div className={containerAnims.animation + " contents--container"}>
-				<ContentContainer className={containerAnims.leftNone}/>
-				<ContentContainer onClick={handleAnimation}  className={containerAnims.left}/>
-				<ContentContainer className={containerAnims.middle} />
-				<ContentContainer onClick={handleAnimation} className={containerAnims.right}/>
-				<ContentContainer className={containerAnims.rightNone}/>
+				<ContentContainer data={data[0]}  className={containerAnims.leftNone}/>
+				<ContentContainer data={data[1]} onClick={handleClick}  className={containerAnims.left}/>
+				<ContentContainer data={data[2]} className={containerAnims.middle} />
+				<ContentContainer data={data[3]} onClick={handleClick} className={containerAnims.right}/>
+				<ContentContainer data={data[4]} className={containerAnims.rightNone}/>
 			</div>
 		</div>
 	)
